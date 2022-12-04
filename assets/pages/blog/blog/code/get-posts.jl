57307a8957ef600ef11deb013279b271d274272d
lwkatzen <69644272+lwkatzen@users.noreply.github.com>
2022-12-04 23:22:13 +0000

    Deploying to gh-pages from  @ 1dfdd56faafcd3e6d3639a50d5da1a019e037ffc 🚀

--- assets/pages/blog/blog/code/get-posts.jl
+++
@@ -1,9 +0,0 @@
-# This file was generated, do not modify it. # hide
-#hideall
-for (root, dirs, files) in walkdir("./pages/blog/posts")
-  for dir in dirs
-    # displays link to each post
-    # chops the "." from the begining of path
-    println("[$dir](" * chop(joinpath(root,dir), head = 1, tail = 0) * ")\n")
-  end
-end
